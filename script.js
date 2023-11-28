3852a8ec7d4d076d4515c497937f4127971a33ad
Mani Garg <92200315+manigarg15@users.noreply.github.com>
2023-11-28 16:33:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let hrotation = 30 * h +  m / 2;
+    let mrotation =  6 * m;
+    let srotation = 6 * s;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
